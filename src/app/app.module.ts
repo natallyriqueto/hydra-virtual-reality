d0b5640dc8ad837403226259fc2bc4251fced1ee
Natally <natallyriqueto@hotmail.com>
2023-06-21 18:01:56 -0400

    add component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HeroComponent } from './hero/hero.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HeroComponent,
+    BenefitsComponent,
+    TechnologiesComponent,
+    RegisterComponent,
+    AboutComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
